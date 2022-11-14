dbb03dfc45f2d352713ea029e68fc8aab50416ef
Nymfeparakit <maxlex047@gmail.com>
2022-11-14 09:06:16 +0300

    Добавлены логи в auth middleware

--- internal/app/middlewares/auth.go
+++ internal/app/middlewares/auth.go
@@ -3,7 +3,9 @@ package middlewares
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 	"gophermart/internal/app/domain"
 	"gophermart/internal/app/services"
 	"net/http"
@@ -23,11 +25,13 @@ func TokenAuthMiddleware(userService UserService, authService AuthService) gin.H
 	return func(c *gin.Context) {
 		tokenHeader := c.Request.Header["Authorization"]
 		if tokenHeader == nil {
+			log.Error().Msg("there is no auth header in request")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		tokenHeaderStr := strings.Split(tokenHeader[0], " ")
 		if len(tokenHeaderStr) != 2 {
+			log.Error().Msg("failed to parse token header")
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
@@ -35,19 +39,23 @@ func TokenAuthMiddleware(userService UserService, authService AuthService) gin.H
 
 		username, err := authService.ParseUserToken(tokenString)
 		if errors.Is(err, services.ErrInvalidAccessToken) {
+			log.Error().Msg(fmt.Sprintf("failed to parse token value: %v", err.Error()))
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		if err != nil {
+			log.Error().Msg(fmt.Sprintf("failed to parse token value: %v", err.Error()))
 			c.AbortWithStatus(http.StatusInternalServerError)
 		}
 
 		user, err := userService.GetUserByLogin(c.Request.Context(), username)
 		if errors.Is(err, services.ErrUserDoesNotExist) {
+			log.Error().Msg(fmt.Sprintf("can not find user with username %s", username))
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 		if err != nil {
+			log.Error().Msg(fmt.Sprintf("can not find user with username %s: %v", username, err.Error()))
 			c.AbortWithStatus(http.StatusInternalServerError)
 		}
 
